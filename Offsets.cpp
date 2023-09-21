031b253dcb81dd234208f551ac59aca8e28cf2c3
Sh3rlock <fhawaa@gmail.com>
2023-09-21 01:45:41 -0700

    1.14.4.8.535064

--- Offsets.cpp
+++ Offsets.cpp
@@ -1,10 +1,9 @@
-/*
-Game    = Farlight 84 (steam)
+UPDATING 338.11MB [steam]
+#Latest : 1.14.4.8.535064
 
-*/
-DWORD Gname   = 0x6645580
-DWORD Gworld  = 0x67b5080
-DWORD Objects = 0x665ddf0 
+DWORD GWorld = 0x6807900; 
+DWORD GNames = 0x6697E00; 
+DWORD GOBJECT = 0x66B0680;
 DWORD PersistentLevel = 0x30; 
 DWORD OwningGameInstance = 0x220; 	
 DWORD LocalPlayers = 0x38;  
@@ -20,11 +19,9 @@ DWORD RelativeRotation = 0x128;
 DWORD ComponentVelocity = 0x140; 
 DWORD bIsCrouched = 0x330; 
 DWORD bIsABot = 0x232;
-DWORD OS = 0x638;
+DWORD OS = 0x640;  //CH
 DWORD RecoilComponent = 0x598;
-DWORD CurrentHealth = 0x580;
-DWORD MaxHealth = 0x584;
+DWORD CurrentHealth = 0x588;  //CH
+DWORD MaxHealth = 0x58c;      //CH
 DWORD PlayerState = 0x248;
-DWORD TeamID = 0xc;
-
-
+DWORD TeamID = 0x240;
