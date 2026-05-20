4b6248fe95ae8d9c107019f45663e7c5be49066e
NULL <khabdualziz@gmail.com>
2026-05-20 02:40:00 -0700

    Update offsets 2.11.0.0
    Updated offsets for GWorld and GNames, and removed outdated entries.

--- Offsets.cpp
+++ Offsets.cpp
@@ -1,27 +1,5 @@
-UPDATING 338.11MB [steam]
-#Latest : 1.14.4.8.535064
-
-DWORD GWorld = 0x6807900; 
-DWORD GNames = 0x6697E00; 
-DWORD GOBJECT = 0x66B0680;
-DWORD PersistentLevel = 0x30; 
-DWORD OwningGameInstance = 0x220; 	
-DWORD LocalPlayers = 0x38;  
-DWORD PlayerController = 0x30; 
-DWORD AcknowledgedPawn = 0x2c0; 
-DWORD PlayerCameraManager = 0x2d8; 
-DWORD CameraCachePrivate = 0x1bf0; 
-DWORD Mesh = 0x288;  
-DWORD StaticMesh = 0x588;  
-DWORD RootComponent = 0x138; 
-DWORD RelativeLocation = 0x11c;
-DWORD RelativeRotation = 0x128;
-DWORD ComponentVelocity = 0x140; 
-DWORD bIsCrouched = 0x330; 
-DWORD bIsABot = 0x232;
-DWORD OS = 0x640;  //CH
-DWORD RecoilComponent = 0x598;
-DWORD CurrentHealth = 0x588;  //CH
-DWORD MaxHealth = 0x58c;      //CH
-DWORD PlayerState = 0x248;
-DWORD TeamID = 0x240;
+[steam]
+#Latest : 2.11.0.0
+  
+[+] GWorld Offset : 0xbca8fd0
+[+] GNames Offset : 0xbb2ed00
